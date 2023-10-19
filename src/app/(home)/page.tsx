eae85d75d1b40cf1be078d213e2bf1e71c98c6eb
Rogério <rogeriopiatek@gmail.com>
2023-10-19 18:50:03 -0300

    feat: add first discount banner to home page

---
+++ src/app/(home)/page.tsx
@@ -0,0 +1,22 @@
+"use client";
+
+import Image from "next/image";
+import Categories from "./components/categories";
+
+export default function Home() {
+  return (
+    <div className="p-5">
+      <Image
+        src="/banner-home-01.png"
+        height={0}
+        width={0}
+        className="h-auto w-full"
+        sizes="100vw"
+        alt="Até 55% de desconto esse mês!"
+      />
+      <div className="mt-8">
+        <Categories />
+      </div>
+    </div>
+  );
+}
