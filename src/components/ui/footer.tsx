82f1d949adf04b6d223d2ebaaca1eeb730a8f360
Rogério <rogeriopiatek@gmail.com>
2023-10-22 00:10:17 -0300

    feat: add Footer component

---
+++ src/components/ui/footer.tsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+  return (
+    <div className="bg-accent px-8 py-4 text-[0.625rem] opacity-75">
+      © 2023 Copyright <span className="font-semibold">Rogério Store</span>
+    </div>
+  );
+};
+
+export default Footer;
--- src/components/ui/header.tsx
+++ src/components/ui/header.tsx
@@ -105,7 +105,7 @@ const Header = () => {
       </Sheet>
 
       <h1 className="text-lg font-semibold">
-        <span className="text-primary">FSW</span> Store
+        <span className="text-primary">Rogério</span> Store
       </h1>
 
       <Button size="icon" variant="outline">
