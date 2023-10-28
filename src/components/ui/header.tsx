bb1620f11a071d56f3c907a4e4b3d1ec4581fd9b
Rogério <rogeriopiatek@gmail.com>
2023-10-28 20:49:50 -0300

    feat: add persistence to cart products

--- src/components/ui/header.tsx
+++ src/components/ui/header.tsx
@@ -94,12 +94,17 @@ const Header = () => {
               </Button>
             )}
 
-            <Link href={"/"}>
-              <Button variant="outline" className="w-full justify-start gap-2">
-                <HomeIcon size={16} />
-                Início
-              </Button>
-            </Link>
+            <SheetClose asChild>
+              <Link href={"/"}>
+                <Button
+                  variant="outline"
+                  className="w-full justify-start gap-2"
+                >
+                  <HomeIcon size={16} />
+                  Início
+                </Button>
+              </Link>
+            </SheetClose>
             <Button variant="outline" className="w-full justify-start gap-2">
               <PercentIcon size={16} />
               Ofertas
