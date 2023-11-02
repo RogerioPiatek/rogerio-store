26112a035fb5e8aa404aa827f24308072224a23b
Rogério <rogeriopiatek@gmail.com>
2023-11-02 01:40:23 -0300

    fix: ensure orders are only shown to logged users

--- src/app/orders/page.tsx
+++ src/app/orders/page.tsx
@@ -7,14 +7,21 @@ import OrderItem from "./components/order-item";
 
 export const dynamic = "force-dynamic";
 
-const OrderPage = async () => {
-  const user = getServerSession(authOptions);
+async function OrderPage() {
+  const session = await getServerSession(authOptions);
 
-  if (!user) return <p>Access denied</p>;
+  if (!session || !session.user) {
+    return (
+      <div className="flex h-full flex-col items-center justify-center gap-2 p-5">
+        <h2 className="font-bold">Acesso Negado!</h2>
+        <p className="text-sm opacity-60">Faça login para ver seus pedidos</p>
+      </div>
+    );
+  }
 
   const orders = await prismaClient.order.findMany({
     where: {
-      userId: (user as any).id,
+      userId: session.user.id,
     },
     include: {
       orderProducts: {
@@ -26,14 +33,12 @@ const OrderPage = async () => {
   });
 
   return (
-    <div className="p-5">
-      <Badge
-        className="w-fit gap-1 border-2 border-primary px-3 py-[0.375rem] text-base uppercase"
-        variant="outline"
-      >
+    <div className="p-5 lg:container lg:mx-auto lg:py-10">
+      <Badge variant="outline">
         <PackageSearchIcon size={16} />
         Meus Pedidos
       </Badge>
+
       <div className="mt-5 flex flex-col gap-5">
         {orders.map((order) => (
           <OrderItem key={order.id} order={order} />
@@ -41,5 +46,6 @@ const OrderPage = async () => {
       </div>
     </div>
   );
-};
+}
+
 export default OrderPage;
